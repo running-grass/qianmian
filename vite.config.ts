f51d1a776359b23098adf2a1cc90b3f2e3e911f7
running-grass <hi@grass.show>
2024-10-23 16:30:39 +0800

    feat: 增加最近一次更新文档

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import fs from 'node:fs'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -10,6 +10,38 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import packageInfo from './package.json'
 
+import { marked } from 'marked'
+import * as cheerio from 'cheerio'
+
+const changeLog = fs.readFileSync('./CHANGELOG.md').toString()
+
+const changeLogHTML = marked.parse(changeLog)
+
+let latestUpdateLog = ''
+
+if (typeof changeLogHTML === 'string') {
+  const $ = cheerio.load(changeLogHTML)
+  let started = false
+  for (const el of $('body').children()) {
+    if (el.tagName === 'h1') {
+      continue
+    }
+
+    if (started && el.tagName === 'h2') {
+      break
+    }
+
+    if (!started && el.tagName === 'h2') {
+      started = true
+    }
+
+    console.log('el', el.tagName, $(el).html())
+
+    latestUpdateLog += $(el).html()
+  }
+}
+
+// console.log('versionLog', versionLog)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -31,7 +63,8 @@ export default defineConfig({
   },
   esbuild: {
     define: {
-      __APP_VERSION__: JSON.stringify(packageInfo.version)
+      __APP_VERSION__: JSON.stringify(packageInfo.version),
+      __APP_LATEST_UPDATE_LOG__: JSON.stringify(latestUpdateLog)
     },
     drop: process.env.NODE_ENV === 'production' ? ['console', 'debugger'] : []
   },
