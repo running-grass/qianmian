764c5812695365464c3f334760ba4ca56b9c0530
running-grass <hi@grass.show>
2024-10-24 17:03:05 +0800

    chore: 增加公安备案号

--- src/apps/other/views/HomePage.vue
+++ src/apps/other/views/HomePage.vue
@@ -11,8 +11,11 @@
         <RouterLink :to="{ name: 'login' }" class="text-blue-500">去登录</RouterLink>
       </p>
     </section>
-    <footer class="w-full text-center py-4 bg-gray-400">
+    <footer class="w-full text-center py-4 bg-gray-400 flex justify-center">
       <a href="https://beian.miit.gov.cn/" target="_blank">京ICP备2024089620号-2</a>
+      <img src="/public/static/icon/beian.png" class="ml-4 mr-2 w-4 object-contain">
+      <a href="https://beian.mps.gov.cn/#/query/webSearch?code=11011502038341" rel="noreferrer"
+        target="_blank">京公网安备11011502038341</a>
     </footer>
   </div>
 </template>
