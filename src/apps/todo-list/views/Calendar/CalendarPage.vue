e35f63275e5c1c826a81ce4cf0e02669acf30671
奔跑的小草 <hi@grass.show>
2024-10-25 14:36:32 +0800

    feat(todolist): 日历视图增加安排任务的机制 (#26)

--- src/apps/todo-list/views/Calendar/CalendarPage.vue
+++ src/apps/todo-list/views/Calendar/CalendarPage.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import { attributeSchduledEnd, attributeSchduledStart, createEntity, getDb, identityTodoItem, todoItemView, useMobile, type TodoItem } from '@/core';
+import interactionPlugin, { Draggable, type DropArg } from '@fullcalendar/interaction';
+
+import { attributeSchduledEnd, attributeSchduledStart, createEntity, entityTable, getDb, identityTodoItem, todoItemView, useMobile, type TodoItem } from '@/core';
 import { myDayjs } from '@/plugins/dayjs';
-import type { CalendarOptions, DateSelectArg, EventInput } from '@fullcalendar/core/index.js';
+import type { CalendarOptions, DateSelectArg, EventClickArg, EventDropArg, EventInput } from '@fullcalendar/core/index.js';
 import zhLocale from '@fullcalendar/core/locales/zh-cn';
 import { computed, ref, watch } from 'vue';
 import TodoItemDetail from '../../components/TodoItemDetail.vue';
 import { changeTodoItemAttribute, deleteTodoItem } from '../../store';
 import FloatPopover from '@/component/FloatPopover.vue';
 import { getTodoItemById } from '../../function';
+import { XCircleIcon } from '@heroicons/vue/24/solid';
+import { useTemplateRef } from 'vue';
+import { RecordId } from 'surrealdb';
 
 const isMobileScreen = useMobile()
 const detailPanelVisible = ref(false)
@@ -18,6 +22,8 @@ const detailPanelVisible = ref(false)
 const selectedTodoItem = ref<TodoItem | null>(null)
 
 
+const fullCalendar = useTemplateRef('full-calendar')
+const draggableElement = useTemplateRef('draggable-element')
 type TodoItemEventInput = EventInput & {
   todoItem: TodoItem
 }
@@ -40,67 +46,134 @@ refreshtodoItems()
 
 const createMode = ref(false)
 
+async function select(info: DateSelectArg) {
+  const id = await createEntity(identityTodoItem.value.id, '')
+  await changeTodoItemAttribute(
+    id,
+    attributeSchduledStart.value.id,
+    info.start
+  )
+  if (!myDayjs(info.end).isSame(myDayjs(info.start).add(1, 'day'))) {
+    await changeTodoItemAttribute(
+      id,
+      attributeSchduledEnd.value.id,
+      info.end
+    )
+  }
+
+  const t = await getTodoItemById(id)
+
+  selectedTodoItem.value = t
+  createMode.value = true
+  detailPanelVisible.value = true
+}
+
+async function eventDrop(info: EventDropArg) {
+  const item: TodoItem = info.event.extendedProps.todoItem
+  console.info(info.event.title + " was dropped on " + myDayjs(info.event.start).toDate(), info.event.end);
+
+  if (info.event.start) {
+    await changeTodoItemAttribute(
+      item.entity_id,
+      attributeSchduledStart.value.id,
+      info.event.start
+    )
+  }
+
+  if (info.event.end) {
+    await changeTodoItemAttribute(
+      item.entity_id,
+      attributeSchduledEnd.value.id,
+      info.event.end
+    )
+  }
+  refreshtodoItems()
+}
+
+function eventClick(info: EventClickArg) {
+  const item = info.event.extendedProps.todoItem
+  selectedTodoItem.value = item
+  detailPanelVisible.value = true
+}
+
+/** 安排任务面板 */
+const scheduleTodoItemPanel = ref(false)
+const unScheduleTodoItems = ref<TodoItem[]>([])
+
+async function refreshUnScheduleTodoItems() {
+  const db = await getDb()
+  const [res] = await db.query<[TodoItem[]]>(`SELECT * FROM ${todoItemView.tb} WHERE done = false AND scheduled_start IS null AND scheduled_end IS NULL`)
+  unScheduleTodoItems.value = res
+}
+const customButtons = {
+  scheduleTask: {
+    text: '安排任务',
+    click: async function () {
+      unScheduleTodoItems.value = []
+      scheduleTodoItemPanel.value = true
+
+      await refreshUnScheduleTodoItems()
+    }
+  }
+}
+
+watch(scheduleTodoItemPanel, (visible) => {
+  let draggable: Draggable | undefined = undefined
+
+  setTimeout(() => {
+    if (visible) {
+      if (!draggableElement.value) {
+        return
+      }
+      draggable = new Draggable(draggableElement.value, {
+        itemSelector: 'li'
+      });
+    } else {
+      draggable && draggable.destroy()
+    }
+
+    if (fullCalendar.value) {
+      fullCalendar.value.getApi().render()
+    }
+  }, 40)
+})
+
 const calendarOptions = computed<CalendarOptions>(() => ({
   plugins: [dayGridPlugin, interactionPlugin],
   timeZone: 'local',
   locale: zhLocale,
   firstDay: 1,
   initialView: 'dayGridMonth',
-  editable: true,
-  selectable: true,
   height: '100%',
   // 待办事项相关
   events: events.value,
-  eventClick: function (info) {
-    const item = info.event.extendedProps.todoItem
-    selectedTodoItem.value = item
-    console.log(item)
-    detailPanelVisible.value = true
+
+  // 点击和拖拽操作
+  editable: true,
+  eventClick,
+  eventDrop,
+
+  // 外部拖拽
+  droppable: true,
+  drop: async function (info: DropArg) {
+    const entityId = info.draggedEl.dataset.entityId
+    if (!entityId) {
+      console.error('draggable: entityId not found')
+      return null
+    }
+    const eId = new RecordId(entityTable.tb, entityId)
+
+    await changeTodoItemAttribute(eId, attributeSchduledStart.value.id, info.date)
+    await Promise.all([refreshtodoItems(), refreshUnScheduleTodoItems()])
   },
+  // 跨日期选中
+  selectable: true,
+  select,
 
-  eventDrop: async function (info) {
-    const item: TodoItem = info.event.extendedProps.todoItem
-    console.info(info.event.title + " was dropped on " + myDayjs(info.event.start).toDate(), info.event.end);
-
-    if (info.event.start) {
-      await changeTodoItemAttribute(
-        item.entity_id,
-        attributeSchduledStart.value.id,
-        info.event.start
-      )
-    }
-
-    if (info.event.end) {
-      await changeTodoItemAttribute(
-        item.entity_id,
-        attributeSchduledEnd.value.id,
-        info.event.end
-      )
-    }
-    refreshtodoItems()
-  },
-
-  select: async function (info: DateSelectArg) {
-    const id = await createEntity(identityTodoItem.value.id, '')
-    await changeTodoItemAttribute(
-      id,
-      attributeSchduledStart.value.id,
-      info.start
-    )
-    if (!myDayjs(info.end).isSame(myDayjs(info.start).add(1, 'day'))) {
-      console.debug('select on ', info.start, info.end)
-      await changeTodoItemAttribute(
-        id,
-        attributeSchduledEnd.value.id,
-        info.end
-      )
-    }
-
-    const t = await getTodoItemById(id)
-
-    selectedTodoItem.value = t
-    createMode.value = true
-    detailPanelVisible.value = true
+  // 自定义按钮
+  customButtons,
+  headerToolbar: {
+    end: 'scheduleTask today prev,next'
   }
 }))
 
@@ -119,14 +192,61 @@ async function onDetailPanelClose() {
   }
   await refreshtodoItems()
 }
+
+
+
+function onItemDragStart(e: DragEvent) {
+  const target = e.target
+  if (!(target instanceof HTMLLIElement) || !e.dataTransfer) {
+    return
+  }
+
+  const itemId = target.dataset.entityId
+  if (!itemId) {
+    console.warn('onstart: no item id')
+    return
+  }
+  e.dataTransfer.dropEffect = "move";
+
+  e.dataTransfer.setData(itemId, itemId)
+
+  target.classList.add('drop-shadow')
+}
+
+function onItemDragEnd(e: DragEvent) {
+
+  const target = e.target
+  if (!(target instanceof HTMLLIElement)) {
+    return
+  }
+
+  target.classList.remove('drop-shadow')
+}
+
 </script>
 <template>
-  <section class="p-2 w-full h-full">
-    <FullCalendar :options="calendarOptions">
+  <section class="w-full h-full flex">
+    <FullCalendar ref="full-calendar" :options="calendarOptions" class="flex-1">
       <!-- <template v-slot:eventContent='arg'>
       <b>{{ arg.event.title }}</b>
     </template> -->
     </FullCalendar>
+    <section class="p-2 flex flex-col w-32 transition-[width] duration-1000" v-if="scheduleTodoItemPanel">
+      <header class="flex flex-row justify-end mb-2">
+        <XCircleIcon class="w-6 h-6 cursor-pointer hover:fill-green-900" @click="scheduleTodoItemPanel = false" />
+      </header>
+      <ul ref="draggable-element" class="flex-1 overflow-y-auto flex flex-col">
+        <li v-for="todoItem in unScheduleTodoItems" :key="todoItem.entity_id.toString()" :draggable="true"
+          @dragstart="onItemDragStart" @dragend="onItemDragEnd" :data-entity-id="todoItem.entity_id.id.toString()"
+          :class="[
+            'py-2 px-4 hover:bg-green-50 flex items-center cursor-pointer break-all ',
+          ]">
+          {{ todoItem.title }}
+        </li>
+
+      </ul>
+
+    </section>
   </section>
 
   <!-- 详情 -->
