13735294a6d647a04ce80867456d1d2a7df1de6d
奔跑的小草 <hi@grass.show>
2024-11-05 11:58:54 +0800

    feat: 增加离线时的提示边框 (#37)

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,26 @@ import { RouterView } from 'vue-router'
 import { AlphaTip } from '@/component'
 import { ElConfigProvider } from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import { useNetwork } from '@vueuse/core';
+
+const { isOnline } = useNetwork()
+
 </script>
 
 <template>
   <el-config-provider :locale="zhCn">
     <AlphaTip />
     <Suspense>
-      <RouterView />
+      <!-- 渐变闪烁的红色边框 -->
+      <RouterView :class="isOnline ? '' : 'app-offline'" />
       <template #fallback> 正在启动中... </template>
     </Suspense>
   </el-config-provider>
 </template>
+<style lang="css" scoped>
+.app-offline {
+  @apply border-8 border-red-500 rounded-lg;
+  @apply transition-all duration-300;
+  @apply animate-pulse;
+}
+</style>
